b8bbfef0e1b5898622196483a84ab3d2b584d9bb
MD ABRAR LABIB <145818625+ASHASchowdhury@users.noreply.github.com>
2024-03-19 14:31:16 +0600

    Update Vanya and Fance.cpp

--- Vanya and Fance.cpp
+++ Vanya and Fance.cpp
@@ -1,1 +1,23 @@
-
+#include <iostream>
+using namespace std;
+ 
+int main() {
+    int t, s;
+    cin >> t >> s;
+ 
+    int count1 = 0, count2 = 0;
+    while (t--) {
+        int a;
+        cin >> a;
+        if (a > s) {
+            count1++;
+        } else {
+            count2++;
+        }
+    }
+ 
+    int tot = count1 * 2 + count2;
+    cout << tot;
+ 
+    return 0;
+}
