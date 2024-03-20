724fe513cff4baba2bcefaff2d6a667c238c213a
MD ABRAR LABIB <145818625+ASHASchowdhury@users.noreply.github.com>
2024-03-20 10:01:06 +0600

    Create Anton and danik.cpp

---
+++ Anton and danik.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+ 
+int t,count1=0,count2=0;
+cin>>t;
+string a;
+cin>>a;
+for(int i=0;i<t;i++){
+ 
+    r    else count2++;
+    }
+    if(count1==count2)cout<<"Friendship";
+    else if(count1>count2)cout<<"Anton";
+    else cout<<"Danik";
+}
