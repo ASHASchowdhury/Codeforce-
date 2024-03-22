4cfec032f7fc5dfb162a617bd4ca0df275c6a7dd
MD ABRAR LABIB <145818625+ASHASchowdhury@users.noreply.github.com>
2024-03-23 02:35:30 +0600

    Create Word Capitalization.cpp

---
+++ Word Capitalization.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+ 
+string a;
+cin>>a;
+ 
+    if(!a.empty()){
+        a[0]=toupper(a[0]);
+    }
+ 
+        cout<<a;
+ 
+ 
+return 0;
+}
