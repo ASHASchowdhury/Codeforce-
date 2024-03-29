5c438db14cadc9dc1894b7466d6b7ff31385b32f
MD ABRAR LABIB <145818625+ASHASchowdhury@users.noreply.github.com>
2024-03-30 00:58:14 +0600

    Create 579A-Raising bectaria.cpp

---
+++ 1000 rating/579A-Raising bectaria.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+ 
+using namespace std;
+ 
+int main() {
+    int x;
+    cin >> x;
+ 
+    int count = 0;
+    while (x) {
+        count += (x & 1); 
+        x >>= 1;         
+    }
+ 
+    cout << count << endl;
+ 
+    return 0;
+}
